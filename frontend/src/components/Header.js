3c9d79590a2e860bc3b15c66835886f48ccdeb72
TimManas <timothy.romero.manas@gmail.com>
2021-02-13 10:18:02 -0500

    Added Base CSS templates

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+const Header = () => {
+  return <Navbar bg='primary'>Header</Navbar>
+}
+
+export default Header
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+  return (
+    <>
+      <h1>ProductScreen</h1>
+    </>
+  )
+}
+
+export default ProductScreen
